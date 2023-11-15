13039560f80b5c0539c920cfc60098cb8cbbc470
Kyrre Havik <kyrremann@gmail.com>
2023-11-15 12:43:07 +0100

    fix: må spesifisere samme repo

--- dags/poc.py
+++ dags/poc.py
@@ -5,6 +5,7 @@ from dataverk_airflow import python_operator
 with DAG(dag_id="skyporten_demo", schedule_interval=None, start_date=datetime(2023, 11, 14), catchup=False) as dag:
     t1 = python_operator(dag=dag,
                          name="demo",
+                         repo="navikt/skyporten-poc",
                          requirements_path="/requirements.txt",
                          script_path="/main.py")
 
